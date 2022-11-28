698a549f6481488dc9d60f5c80737563db275b67
Peter John Tumang <202080207@psu.palawan.edu.ph>
2022-11-28 09:10:30 +0800

    inc directory done

---
+++ cover-verbose-memory/contacts.php
@@ -0,0 +1,12 @@
+<?php include 'inc/header.php'; ?>
+
+<main role="main" class="inner cover">
+    <h1 class="cover-heading">CONTACT</h1>
+    <p class="lead">You may send me an email at 202080207@psu.palawan.edu.ph</p>
+    <p class="lead">OR</p>
+    <p class="lead">You may call on 09281929035</p>
+    <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+    </p>
+</main>
+
+<?php include 'inc/footer.php'; ?>
---
+++ cover-verbose-memory/index.php
@@ -0,0 +1,11 @@
+<?php include 'inc/header.php' ?>
+
+<main role="main" class="inner cover">
+    <h1 class="cover-heading">HOME</h1>
+    <p class="lead">Finally working!</p>
+    <p class="lead">
+        <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+    </p>
+</main>
+
+<?php include 'inc/footer.php' ?>
